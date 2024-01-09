9442d256742b56e70d516a67e4bc1236102855ff
Kiran-AWS-Git <139952431+Kiran-AWS-Git@users.noreply.github.com>
2024-01-09 15:54:06 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
-class Demo{
-	public static void main(String[] args){
-		System.out.println("This is a demo program.");
-		System.out.println("Git is very easy to Operate");
+class Demo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is Demo file in Kiran-AWS-DevOps/Maven-Repo Git repository.");
 	}
 }
