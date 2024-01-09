16714ae96e1771f4df30ed7ac3d5042154739c7a
Kiran-AWS-Git <139952431+Kiran-AWS-Git@users.noreply.github.com>
2024-01-09 15:55:35 +0530

    Update Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -1,5 +1,7 @@
-class Demo2{
-	public static void main(String[] args){
-		System.out.println("Git Demo");
+class Demo2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is Demo2 java file in Kiran-AWS-DevOps/Maven-Repo Git repository");
 	}
 }
